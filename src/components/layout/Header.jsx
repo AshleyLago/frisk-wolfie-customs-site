071126c8af32e304b23d3dc00db3ed4a84de035d
Ashley Lago <ashl3ylag0@gmail.com>
2025-02-11 17:56:35 -0500

    Replaced Placeholder Logos in Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,21 +1,58 @@
-import PlaceholderFwcLogo from '../../assets/placeholder-fwc-logo.png'
+import { useState, useEffect } from 'react';
+import FwcLogo from "../../assets/fwc-logo.png";
+import FwcIntroAnimation from "../../assets/fwc-intro-animation.gif";
+import FwcIdleAnimation from "../../assets/fwc-idle-animation.gif";
 import { NavLink } from 'react-router-dom';
 
 export default function Header() {
+    const [currentImage, setCurrentImage] = useState(FwcIntroAnimation);
+
+    /* Preloaded images for seamless transitions */
+    useEffect(() => {
+        const images = [FwcLogo, FwcIntroAnimation, FwcIdleAnimation];
+        images.forEach(src => {
+            const img = new Image();
+            img.src = src;
+        });
+    }, []);
+
+    /* Plays FWC Intro Animation on page load */
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setCurrentImage(FwcLogo);
+        }, 8199);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    /* Plays the Idle Animation every minute */
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage(FwcIdleAnimation);
+
+            const timeout = setTimeout(() => {
+                setCurrentImage(FwcLogo);
+            }, 1600);
+
+            return () => clearTimeout(timeout);
+        }, 10000); // Change to 60 seconds when complete
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <header className="mt-5 mb-10 mx-10">
-
             {/* FWC Logo */}
             <section className="flex flex-col items-center relative w-full mb-10">
                 <img
-                    src={PlaceholderFwcLogo}
+                    src={currentImage}
                     alt="Frisk Wolfie's Customs Logo"
                     className="max-w-full h-auto w-[40%] lg:w-[30%] xl:w-[20%] min-w-[200px] lg:min-w-[250px] xl:min-w-[300px]"
                 />
             </section>
 
             {/* Navigation Buttons */}
-            <section className="flex flex-wrap justify-center w-full gap-4 lg:gap-6 xl:gap-8">
+            <section className="flex flex-wrap justify-center w-full gap-4 lg:gap-6 xl:gap-8" style={{ fontFamily: '"Baloo 2", sans-serif', fontWeight: 800 }}>
                 <NavLink to="/" className="button-navlink text-base lg:text-lg xl:text-xl p-2 lg:p-3 xl:p-4">
                     Home
                 </NavLink>
@@ -35,7 +72,6 @@ export default function Header() {
                     Etsy Shop
                 </a>
             </section>
-
         </header>
-    )
+    );
 }
